3641078993be77b09a6a39d7ea1b11aeb373d6ca
rafiudd <sirin6867@gmail.com>
2020-01-16 12:00:07 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('helpers/jwt');
+const errorHandler = require('helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+
+
+app.get('/', function (req, res) {
+    res.json({
+        status: 200,
+        message: 'API dah jalan created with lope frome me :D'
+    });
+});
+app.use('/api/users', require('./modules/users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8081;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
---
+++ modules/users/users.model.js
@@ -0,0 +1,16 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String },
+    fullname: { type: String },
+    email: { type: String },
+    password: { type: String },
+    phone: { type: String },
+    role: { type: String },
+    created_at: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
